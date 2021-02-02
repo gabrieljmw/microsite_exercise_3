a3a093d9a727150a4a5bc45458600ae1419d1e47
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2021-02-01 21:51:52 -0500

    Rebuilding from scratch to solidify understanding

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-function start() {
-
-}
-
-$(window).on("load", start);
-
 function loadpage(e) {
-
     e.preventDefault();
-    $("#menu a.active").removeClass("active");
+    $(".nav .nav-link.active").removeClass("active");
     $(this).addClass("active");
 
-    $("#content").load("about.html");
-
+    let href = $(this).attr('href');
+    $(".content").load(href);
 }
 
-$(document).on("click", "#menu a", loadpage);
+$(document).on("click", ".nav .nav-item .nav-link", loadpage);
